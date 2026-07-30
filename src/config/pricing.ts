982a85cf6423df1136c316f2ec1681a3abefa26c
SKOZX <tranhuybao000@gmail.com>
2026-07-30 09:29:47 +0700

    Adjust VPS promotion prices

--- src/config/pricing.ts
+++ src/config/pricing.ts
@@ -8,11 +8,11 @@ export const pricingConfig = {
     "bruh-vps-1-1": { originalPrice: null, salePrice: 59000 },
     "bruh-vps-1-2": { originalPrice: null, salePrice: 79000 },
     "bruh-vps-2-2": { originalPrice: null, salePrice: 99000 },
-    "bruh-vps-2-4": { originalPrice: null, salePrice: 149000 },
-    "bruh-vps-2-8": { originalPrice: null, salePrice: 239000 },
-    "bruh-vps-4-8": { originalPrice: null, salePrice: 299000 },
-    "bruh-vps-4-16": { originalPrice: null, salePrice: 349000 },
-    "bruh-vps-8-16": { originalPrice: null, salePrice: 599000 },
+    "bruh-vps-2-4": { originalPrice: 179000, salePrice: 119000 },
+    "bruh-vps-2-8": { originalPrice: 239000, salePrice: 179000 },
+    "bruh-vps-4-8": { originalPrice: 299000, salePrice: 239000 },
+    "bruh-vps-4-16": { originalPrice: 499000, salePrice: 349000 },
+    "bruh-vps-8-16": { originalPrice: 899000, salePrice: 599000 },
   },
 } satisfies { promotions: Record<string, Promotion> };
 
